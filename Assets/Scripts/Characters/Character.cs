565d874090246fd9e7a38a089d6e3156cf5b6a02
Valarie Adams <deviantdear@gmail.com>
2020-10-30 23:48:06 -0700

    Added 2D Tech Demos and 2C extras to the project
    added some basic scripts that need to be completed and #rd party assets that are not meant to be distributed

---
+++ Assets/Scripts/Characters/Character.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    protected void TakeDamage()
+    {
+
+    }
+
+    protected void Heal ()
+    {
+
+    }
+
+
+
+}
---
+++ Assets/Scripts/Characters/CharacterData.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Sprites;
+
+[CreateAssetMenu(fileName = "CharacterData")]
+public class CharacterData : ScriptableObject
+{
+    public string Name;
+    public Texture2D Profile;
+
+    public string Class;
+    public string Race;
+    public string Background;
+    public string Alignment;
+
+    [SerializeField]
+    private int HP;
+    [SerializeField]
+    private int AC;
+    [SerializeField]
+    private int Speed;
+    [SerializeField]
+    private int XP;
+
+    [SerializeField]
+    private int Constitution;
+    [SerializeField]
+    private int Wisdom;
+    [SerializeField]
+    private int Strength;
+    [SerializeField]
+    private int Intelligence;
+    [SerializeField]
+    private int Dexterity;
+    [SerializeField]
+    private int Charisma;
+
+}
